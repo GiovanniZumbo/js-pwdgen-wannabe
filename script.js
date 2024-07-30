b2e0b616ac696cbc7eea5c268981ffc34db6d993
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-07-30 15:26:10 +0200

    Fase elaborazione

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 1. Recupero dell'elemento di interesse nel DOM / Get the element of interest from the DOM
 2. Chiedo il nome all'utente / Request the first name of the user
 3. Chiedo il cognome all'utente / Request the last name of the user 
-4. Chiedo il colore preferito all'utente / Request the user's favourite colour
+4. Chiedo il colore preferito all'utente / Request user's favourite colour
 5. Creo una variabile che unisca tutti e 3 i dati con il numero 21 / Make a variable linking those three datas with a the default number "21"
 6. Inserisco il risultato nell'elemento del DOM / Print the result in the element of interest
 
@@ -36,6 +36,9 @@ console.log(favouriteColour, typeof favouriteColour);
 
 // 5. Creo una variabile che unisca tutti e 3 i dati con il numero 21
 
-// ? FASE OUTPUT
+const gatheredData = `${firstName}${lastName}${favouriteColour}21`;
+console.log(gatheredData, typeof gatheredData);
 
+
+// ? FASE OUTPUT
 
