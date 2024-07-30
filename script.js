f8094ea027a4503141413d43c276966306aaaf0d
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-07-30 14:57:53 +0200

    Scaletta JS

---
+++ script.js
@@ -0,0 +1,12 @@
+// Chiedere all'utente Nome, cognome e colore preferito e crearci una password 
+
+/*
+1. Recupero dell'elemento di interesse nel DOM
+2. Chiedo il nome all'utente
+3. Chiedo il cognome all'utente
+4. Chiedo il colore preferito all'utente
+5. Creo una variabile che unisca tutti e 3 i dati con il numero 21
+6. Inserisco il risultato nell'elemento del DOM
+
+*/
+
