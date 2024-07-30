69b09581265c79b4743a9aec7c8d834c3114cea7
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-07-30 15:11:46 +0200

    fase preparatoria

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-// Chiedere all'utente Nome, cognome e colore preferito e crearci una password 
+// Chiedere all'utente Nome, cognome e colore preferito e crearci una password
 
 /*
-1. Recupero dell'elemento di interesse nel DOM
-2. Chiedo il nome all'utente
-3. Chiedo il cognome all'utente
-4. Chiedo il colore preferito all'utente
-5. Creo una variabile che unisca tutti e 3 i dati con il numero 21
-6. Inserisco il risultato nell'elemento del DOM
+1. Recupero dell'elemento di interesse nel DOM / Get the element of interest from the DOM
+2. Chiedo il nome all'utente / Request the first name of the user
+3. Chiedo il cognome all'utente / Request the last name of the user 
+4. Chiedo il colore preferito all'utente / Request the user's favourite colour
+5. Creo una variabile che unisca tutti e 3 i dati con il numero 21 / Make a variable linking those three datas with a the default number "21"
+6. Inserisco il risultato nell'elemento del DOM / Print the result in the element of interest
 
 */
+
+// ? FASE PREPARATORIA 
+// 1. Recupero dell'elemento di interesse nel DOM
+
+const resultElement = document.getElementById('result');
+console.log(resultElement);
+
+// ? FASE RACCOLTA
+// 2. Chiedo il nome all'utente
+
+
+const firstName = ('Inserisci il tuo nome , John')
+
+// ? FASE ELABORAZIONE
+
+// ? FASE OUTPUT
+
 
